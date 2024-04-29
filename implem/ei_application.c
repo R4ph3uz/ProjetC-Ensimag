20747bf97d9478669737d8dae35eb4d21773680f
HAVET LEONARD <leonard.havet@grenoble-inp.org>
2024-04-29 15:11:33 +0200

    Etape A1 : app_create avec quasi rien , beacoup de code vide ajoutée dans implem , et modification du CMakeList en donnant accés a application, placer, widget configure et widget .c

---
+++ implem/ei_application.c
@@ -0,0 +1,37 @@
+#include "ei_implementation.h"
+
+void ei_app_create(ei_size_t main_window_size, bool fullscreen)
+{
+    hw_init();
+    hw_create_window(main_window_size, fullscreen);
+}
+
+void ei_app_free(void)
+{
+
+}
+
+void ei_app_run(void)
+{
+    getchar();
+}
+
+void ei_app_invalidate_rect(const ei_rect_t* rect)
+{
+
+}
+
+void ei_app_quit_request(void)
+{
+
+}
+
+ei_widget_t ei_app_root_widget(void)
+{
+
+}
+
+ei_surface_t ei_app_root_surface(void)
+{
+    
+}
---
+++ implem/ei_widget.c
@@ -0,0 +1,14 @@
+#include "ei_implementation.h"
+
+ei_widget_t		ei_widget_create		(ei_const_string_t	class_name,
+							 ei_widget_t		parent,
+							 ei_user_param_t	user_data,
+							 ei_widget_destructor_t destructor)
+                             {
+
+                             }
+
+
+
+
+
