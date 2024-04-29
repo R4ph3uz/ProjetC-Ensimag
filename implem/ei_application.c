98eda626c8b19eb416ed1b8143ad0ef510f4212e
Raphael E <raphael.euzeby@grenoble-inp.org>
2024-04-29 16:20:25 +0200

    application de base

--- implem/ei_application.c
+++ implem/ei_application.c
@@ -1,18 +1,9 @@
-//
-// Created by legrandh on 29/04/24.
-//
-
 #include "ei_application.h"
-#include "hw_interface.h"
-#include "ei_geometrymanager.h"
 
-void ei_app_create(ei_size_t main_window_size, bool fullscreen){
+void ei_app_create(ei_size_t main_window_size, bool fullscreen)
+{
     hw_init();
-//    ei_widgetclass_register();
-//    ei_geometrymanager_register();
     hw_create_window(main_window_size, fullscreen);
-
-
 }
 
 /* ----------------------------------------------------------------- */
@@ -24,19 +15,15 @@ void ei_app_run(void){
 
 /* ----------------------------------------------------------------- */
 
-void ei_app_free(void){
+void ei_app_free(void)
+{
 
 }
-/* ----------------------------------------------------------------- */
-
-void ei_app_quit_request(void){
-    hw_quit();
-}
-
 
 /* ----------------------------------------------------------------- */
 
-ei_widget_t ei_app_root_widget(void) {
+void ei_app_quit_request(void)
+{
 
 }
 
