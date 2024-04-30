f0908982192df896e19726a6c667e80e903f47f9
HAVET LEONARD <leonard.havet@grenoble-inp.org>
2024-04-30 16:50:19 +0200

    geometrymanager et placer début

--- implem/ei_geometrymanager.c
+++ implem/ei_geometrymanager.c
@@ -2,25 +2,53 @@
 
 /*-------------------------------------------------------------------------------------------------------*/
 
-void			ei_geometry_run_finalize(ei_widget_t widget, ei_rect_t* new_screen_location){
+struct ei_impl_geom_param_t
+{
+    ei_anchor_t*		anchor;
+    int*			x;
+    int*			y;
+    int*			width;
+    int*			height;
+    float*			rel_x;
+    float*			rel_y;
+    float*			rel_width;
+    float*			rel_height;
+};
+
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+size_t		ei_geom_param_size()
+{
+    size_t i=9;
+    return i;
+}
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+void			ei_geometry_run_finalize(ei_widget_t widget, ei_rect_t* new_screen_location)
+{
 
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
-void			ei_geometrymanager_register	(ei_geometrymanager_t* geometrymanager){
+void			ei_geometrymanager_register	(ei_geometrymanager_t* geometrymanager)
+{
 
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
-ei_geometrymanager_t*	ei_geometrymanager_from_name	(ei_geometrymanager_name_t name){
+ei_geometrymanager_t*	ei_geometrymanager_from_name	(ei_geometrymanager_name_t name)
+{
 
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
-void			ei_geometrymanager_unmap	(ei_widget_t widget){
+void			ei_geometrymanager_unmap	(ei_widget_t widget)
+{
 
 }
 
