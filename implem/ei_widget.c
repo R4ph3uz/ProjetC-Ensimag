ec38798bb81c14efb00c2e19905ceed3cee9a35b
Raph <r4ph3uz-github@yahoo.com>
2024-04-30 20:50:03 +0200

    élaboration widget_destroy

--- implem/ei_widget.c
+++ implem/ei_widget.c
@@ -67,7 +67,10 @@ ei_widget_t		ei_widget_create		(ei_const_string_t	class_name,
 
 void			ei_widget_destroy		(ei_widget_t		widget)
 {
-
+    // destroy children ?
+    // effacer de l'ecran
+    //detruire (comment ?)
+    (*(widget->wclass->releasefunc))(widget); // ici libère la mémoire
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
@@ -82,4 +85,5 @@ bool	 		ei_widget_is_displayed		(ei_widget_t		widget)
 ei_widget_t		ei_widget_pick			(ei_point_t*		where)
 {
 
+    return NULL; // if root window
 }
