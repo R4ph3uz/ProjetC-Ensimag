00fe7b787c808def7c9207b0c930f7d0b16c5100
Raphael E <raphael.euzeby@grenoble-inp.org>
2024-04-30 15:08:30 +0200

    fonction frame_configure

--- implem/ei_widget_configure.c
+++ implem/ei_widget_configure.c
@@ -1,5 +1,7 @@
 #include "ei_geometrymanager.h"
-
+#include "ei_implementation.h"
+#include "ei_application.h"
+#include "widget_class/ei_frame.h"
 /*-------------------------------------------------------------------------------------------------------*/
 
 void			ei_frame_configure		(ei_widget_t		widget,
@@ -15,7 +17,18 @@ void			ei_frame_configure		(ei_widget_t		widget,
                                            ei_rect_ptr_t*		img_rect,
                                            ei_anchor_t*		img_anchor)
 {
-
+    widget->requested_size=*requested_size;
+    ei_frame_t frame = (ei_frame_t) widget;
+    frame->color=(ei_color_t*) color;
+    frame->border_width= border_width;
+    frame->relief = relief;
+    frame->text=text;
+    frame->text_font=text_font;
+    frame->text_color=text_color;
+    frame->text_anchor=text_anchor;
+    frame->img=img;
+    frame->img_rect=img_rect;
+    frame->img_anchor=img_anchor;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
@@ -36,7 +49,18 @@ void			ei_button_configure		(ei_widget_t		widget,
 							 ei_callback_t*		callback,
 							 ei_user_param_t*	user_param)
 {
-
+//    widget->requested_size=*requested_size;
+//    ei_button_t button = (ei_button_t) widget;
+//    button->color=(ei_color_t*) color;
+//    button->border_width= border_width;
+//    button->relief = relief;
+//    button->text=text;
+//    button->text_font=text_font;
+//    button->text_color=text_color;
+//    button->text_anchor=text_anchor;
+//    button->img=img;
+//    button->img_rect=img_rect;
+//    button->img_anchor=img_anchor;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
