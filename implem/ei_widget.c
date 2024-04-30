471eed3f75f3b70005b17b272f6a7313f5849620
Raphael E <raphael.euzeby@grenoble-inp.org>
2024-04-30 13:53:02 +0200

    ajout widget

--- implem/ei_widget.c
+++ implem/ei_widget.c
@@ -1,13 +1,46 @@
 #include "ei_geometrymanager.h"
-
+#include "ei_implementation.h"
 /*-------------------------------------------------------------------------------------------------------*/
-
+extern ei_impl_widget_t ARBRE_WIDGET;
+static int PICKID=0;
 ei_widget_t		ei_widget_create		(ei_const_string_t	class_name,
                                             ei_widget_t		parent,
                                             ei_user_param_t	user_data,
                                             ei_widget_destructor_t destructor)
 {
+    ei_impl_widget_t* widget=malloc(sizeof(ei_impl_widget_t));
+    widget->wclass = ei_widgetclass_from_name(class_name);
+    widget->pick_id=PICKID; // < Id of this widget in the picking offscreen.
+    ei_color_t* couleur= malloc(sizeof(ei_color_t));
+    couleur->red=(PICKID*100)%255;
+    couleur->green=(PICKID*100)%255;
+    couleur->blue=(PICKID*100)%255;
+    couleur->alpha=(PICKID*100)%255;
+    PICKID++;
+    widget->pick_color = couleur;//< pick_id encoded as a color
+    widget->user_data = user_data;
+    widget->destructor = destructor;
 
+//  /* Widget Hierarchy Management */
+    widget->parent= parent;
+    parent->children_tail->next_sibling=widget;
+    parent->children_tail= widget;
+    widget->children_head=NULL;
+    widget->children_tail=NULL;
+    widget->next_sibling= NULL;///< Pointer to the next child of this widget's parent widget.
+
+//  /* Geometry Management */
+    widget->geom_params = NULL;///< Pointer to the geometry management parameters for this widget. If NULL, the widget is not currently managed and thus, is not displayed on the screen.
+
+    widget->requested_size.height=0;///< See \ref ei_widget_get_requested_size.
+    widget->requested_size.width=0;///< See \ref ei_widget_get_requested_size.
+    widget->screen_location.top_left.x=0;///< See \ref ei_widget_get_screen_location.
+    widget->screen_location.top_left.y=0;///< See \ref ei_widget_get_screen_location.
+    widget->screen_location.size.width=0;///< See \ref ei_widget_get_screen_location.
+    widget->screen_location.size.height=0;///< See \ref ei_widget_get_screen_location.
+
+    widget->content_rect=NULL;	///< See ei_widget_get_content_rect. By defaults, points to the screen_location.
+    return widget;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
--- implem/ei_widget_attributes.c
+++ implem/ei_widget_attributes.c
@@ -1,85 +1,88 @@
 #include "ei_geometrymanager.h"
-
+#include "ei_implementation.h"
 /*-------------------------------------------------------------------------------------------------------*/
 
 ei_widgetclass_t*	ei_widget_get_class		(ei_widget_t		widget)
 {
-
+    return widget->wclass;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
 const ei_color_t*	ei_widget_get_pick_color	(ei_widget_t		widget)
 {
-    
+    return widget->pick_color;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
 ei_widget_t 		ei_widget_get_parent		(ei_widget_t		widget)
 {
-    
+    return widget->parent;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
 ei_widget_t 		ei_widget_get_first_child	(ei_widget_t		widget)
 {
-    
+    return widget->children_head;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
 ei_widget_t 		ei_widget_get_last_child	(ei_widget_t		widget)
 {
-    
+    return widget->children_tail;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
 ei_widget_t 		ei_widget_get_next_sibling	(ei_widget_t		widget)
 {
-    
+    return widget->next_sibling;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
 void*			ei_widget_get_user_data		(ei_widget_t		widget)
 {
-    
+    return widget->user_data;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
 const ei_size_t*	ei_widget_get_requested_size	(ei_widget_t		widget)
 {
-    
+    const ei_size_t* res = &widget->requested_size;
+    return res;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
 void	 		ei_widget_set_requested_size	(ei_widget_t		widget, ei_size_t 		requested_size)
 {
-    
+    widget->requested_size=requested_size;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
 const ei_rect_t*	ei_widget_get_screen_location	(ei_widget_t		widget)
 {
-    
+    const ei_rect_t* res = &widget->screen_location;
+    return res;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
 const ei_rect_t*	ei_widget_get_content_rect	(ei_widget_t		widget)
 {
-    
+    const ei_rect_t* res = widget->content_rect;
+    return res;
 }
 
 /*-------------------------------------------------------------------------------------------------------*/
 
 void	 		ei_widget_set_content_rect	(ei_widget_t		widget, const ei_rect_t*	content_rect)
 {
-    
+    widget->content_rect=(ei_rect_t*) content_rect;
 }
