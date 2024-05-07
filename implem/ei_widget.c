558c276e5b764c5b25719cb4b087b37151c10ab4
HAVET LEONARD <leonard.havet@grenoble-inp.org>
2024-05-07 10:00:10 +0200

    Gestion des fenetres qui passent l'une devant l'autre , /!\ Attention il y a une nouvelle segfault plutôt rare /!\

--- implem/ei_widget.c
+++ implem/ei_widget.c
@@ -73,7 +73,6 @@ void			ei_widget_destroy		(ei_widget_t		widget)
     //détruit les enfants
 
     ei_widget_t prec=widget->parent;
-    ei_widget_t prec2=widget->parent;
     ei_widget_t suiv=widget->next_sibling;
     if (prec->children_head!=widget)
     {
@@ -91,6 +90,10 @@ void			ei_widget_destroy		(ei_widget_t		widget)
     else
     {
         prec->children_head=suiv;
+        if(suiv==NULL)
+        {
+            prec->children_tail=NULL;
+        }
     }
     while(widget->children_head){
         ei_widget_t prochain = widget->children_head->next_sibling;
