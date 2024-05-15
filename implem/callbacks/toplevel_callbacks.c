fd6b8b0662df068f44bc7e396882d8d4c372236d
leonard <leonard.havet@grenoble_inp.fr>
2024-05-16 00:42:21 +0200

    Toplevel redimension prend en compte les resizable x , x ou both

--- implem/callbacks/toplevel_callbacks.c
+++ implem/callbacks/toplevel_callbacks.c
@@ -48,8 +48,14 @@ bool toplevel_up_click_handler(ei_widget_t widget, ei_event_t* event, ei_user_pa
 
 bool toplevel_mouse_mouve_handler(ei_widget_t widget, ei_event_t* event, ei_user_param_t user_param) {
     ei_toplevel_t toplevel = (ei_toplevel_t) user_param;
-    *toplevel->widget.geom_params->width =(int) fmax((*toplevel->min_size)->width , *toplevel->widget.geom_params->width+ event->param.mouse.where.x - toplevel->whereButtonDown.x)  ;
-    *toplevel->widget.geom_params->height =(int) fmax((*toplevel->min_size)->height , *toplevel->widget.geom_params->height+ event->param.mouse.where.y - toplevel->whereButtonDown.y)  ;
+    if (*toplevel->resizable==ei_axis_x ||*toplevel->resizable==ei_axis_both)
+    {
+        *toplevel->widget.geom_params->width =(int) fmax((*toplevel->min_size)->width , *toplevel->widget.geom_params->width+ event->param.mouse.where.x - toplevel->whereButtonDown.x)  ;
+    }
+    if (*toplevel->resizable==ei_axis_y ||*toplevel->resizable==ei_axis_both)
+    {
+        *toplevel->widget.geom_params->height =(int) fmax((*toplevel->min_size)->height , *toplevel->widget.geom_params->height+ event->param.mouse.where.y - toplevel->whereButtonDown.y)  ;
+    }
     toplevel->whereButtonDown = event->param.mouse.where;
 
     return true;
@@ -70,8 +76,8 @@ bool toplevel_move_up_click_handler(ei_widget_t widget, ei_event_t* event, ei_us
 
 bool toplevel_move_mouse_mouve_handler(ei_widget_t widget, ei_event_t* event, ei_user_param_t user_param) {
     ei_toplevel_t toplevel = (ei_toplevel_t) user_param;
-    *toplevel->widget.geom_params->x += event->param.mouse.where.x - toplevel->whereButtonDown.x  ;
-    *toplevel->widget.geom_params->y += event->param.mouse.where.y - toplevel->whereButtonDown.y  ;
+        *toplevel->widget.geom_params->x += event->param.mouse.where.x - toplevel->whereButtonDown.x  ;
+        *toplevel->widget.geom_params->y += event->param.mouse.where.y - toplevel->whereButtonDown.y;
     toplevel->whereButtonDown = event->param.mouse.where;
     return true;
 }
